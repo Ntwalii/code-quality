da6a98fc08ef396b9923ffe13ae67175d5ac49a7
Aubin Ntwali <119161688+Ntwalii@users.noreply.github.com>
2024-08-22 17:05:42 +0200

    Changed the function to an arrow function

--- Exercise-5.js
+++ Exercise-5.js
@@ -1,4 +1,4 @@
-function calculateFactorial(number){
+const calculateFactorial=(number)=>{
         if(number===0||number===1) return 1
         return number*calculateFactorial(number-1)
         }
