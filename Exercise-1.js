ac18dc1a54f712b4343c805ea75c60dedc20ae8e
ntwalii <ntwaliaubin@gmail.com>
2024-08-22 10:22:27 +0200

    Code formatted

--- Exercise-1.js
+++ Exercise-1.js
@@ -1,4 +1,4 @@
-const calculateAverage=(numbersArray)=>{
-    const sumOfNumbers=numbersArray.reduce((acc,element)=>acc+element,0)
-    return sumOfNumbers/numbersArray.length
-}
+const calculateAverage = (numbersArray) => {
+  const sumOfNumbers = numbersArray.reduce((acc, element) => acc + element, 0);
+  return sumOfNumbers / numbersArray.length;
+};
